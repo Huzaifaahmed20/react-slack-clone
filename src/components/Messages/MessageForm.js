126f4013e19421530692bcaa9cd9101593ade8b1
Huzaifa Ahmed <ahmedhuzaifa13@gmail.com>
2020-02-07 17:42:50 +0500

    upload media modal

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -2,6 +2,8 @@ import React, { useReducer } from 'react';
 import { Segment, Button, Input } from 'semantic-ui-react';
 
 import firebase from '../../firebase';
+import FileModal from './FileModal';
+
 const reducer = (prevState, updatedProperty) => ({
   ...prevState,
   ...updatedProperty
@@ -12,11 +14,12 @@ const MessageForm = ({ currentChannel, currentUser }) => {
     message: '',
     loading: false,
     errors: [],
+    modal: false,
     messageRef: firebase.database().ref('messages')
   };
 
   const [state, setState] = useReducer(reducer, initialState);
-  const { message, messageRef, errors, loading } = state;
+  const { message, messageRef, errors, loading, modal } = state;
   const handleChange = ev => setState({ [ev.target.name]: ev.target.value });
 
   const createMessage = () => {
@@ -53,6 +56,9 @@ const MessageForm = ({ currentChannel, currentUser }) => {
     }
   };
 
+  const handleOpenModal = () => setState({ modal: true });
+  const handleCloseModal = () => setState({ modal: false });
+
   return (
     <Segment className="message__form">
       <Input
@@ -78,11 +84,13 @@ const MessageForm = ({ currentChannel, currentUser }) => {
           icon="edit"
         />
         <Button
+          onClick={handleOpenModal}
           color="teal"
           content="Upload Media"
           labelPosition="right"
           icon="cloud upload"
         />
+        <FileModal modal={modal} closeModal={handleCloseModal} />
       </Button.Group>
     </Segment>
   );
